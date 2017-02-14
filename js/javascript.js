5e22539b89c885f8d81792776ac85fa0ac82675c
Felipe <f.martinez11@uniandes.edu.co>
2017-02-13 20:23:19 -0500

    Cambios, un poco mas dinamica.

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,17 @@
+var activarWayPoints = true;
 var contenedor = $('.primera-seccion');
 contenedor.waypoint(
   function (direction)
   {
-    if(direction == 'down')
+    if(activarWayPoints)
     {
-      contenedor.addClass('featurette-fade');
-      console.log("point");
-    }else
-    {
-      contenedor.removeClass('featurette-fade');
-      console.log("point");
+      if(direction == 'down')
+      {
+        contenedor.addClass('featurette-fade');
+      }else
+      {
+        contenedor.removeClass('featurette-fade');
+      }
     }
   },{offset:'80%'});
 
@@ -17,13 +19,38 @@ contenedor.waypoint(
   contenedor2.waypoint(
     function (direction)
     {
-      if(direction == 'down')
+      if(activarWayPoints)
       {
-        contenedor2.addClass('featurette-fade');
-        console.log("point");
-      }else
-      {
-        contenedor2.removeClass('featurette-fade');
-        console.log("point");
+        if(direction == 'down')
+        {
+          contenedor2.addClass('featurette-fade');
+        }else
+        {
+          contenedor2.removeClass('featurette-fade');
+        }
       }
     },{offset:'80%'});
+
+ var elementoAcademicoEscondido = $('.mas-academico');
+ var mostrarElementoAcademico = function ()
+ {
+   document.getElementById('estudios').style.display='none';
+   document.getElementById('sobre_mi').style.display='none';
+   document.getElementById('services').style.display='none';
+   document.getElementById('encabezado').style.display='none';
+   document.getElementById('nav_lista').style.display='none';
+   document.getElementById('nav_inicio').style.display='none';
+   document.getElementById('nav_volver').style.display='block';
+   document.getElementById('mas_academico').style.display = 'block';
+ }
+ var ocultarElementoAcademico = function ()
+ {
+   document.getElementById('estudios').style.display='block';
+   document.getElementById('sobre_mi').style.display='block';
+   document.getElementById('services').style.display='block';
+   document.getElementById('encabezado').style.display='block';
+   document.getElementById('nav_lista').style.display='block';
+   document.getElementById('nav_inicio').style.display='block';
+   document.getElementById('nav_volver').style.display='none';
+   document.getElementById('mas_academico').style.display = 'none';
+ }
